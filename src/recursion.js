6d38674b21852c97d3631d1c56b904e86465deea
Alena Kharytonchyk <alena.kharytonchyk@outlook.com>
2018-10-28 00:52:11 +0300

    fix: task three solution logic

--- src/recursion.js
+++ src/recursion.js
@@ -2,15 +2,18 @@ function recursion(tree, arr, depth) {
   const currentDepth = depth || 0;
   const currentArray = arr || [];
 
-  if (currentArray[currentDepth] === undefined) {
-    currentArray[currentDepth] = [tree.value];
-  } else {
-    currentArray[currentDepth].push(tree.value);
+  if (tree.value !== undefined) {
+    if (currentArray[currentDepth] === undefined) {
+      currentArray[currentDepth] = [tree.value];
+    } else {
+      currentArray[currentDepth].push(tree.value);
+    }
   }
 
   if (tree.left !== undefined) {
     recursion(tree.left, currentArray, currentDepth + 1);
   }
+
   if (tree.right !== undefined) {
     recursion(tree.right, currentArray, currentDepth + 1);
   }
